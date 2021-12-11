25f9d9a1af25f899fdf76046ad463f61fee32363
Guxim <guxim.selmani1@hotmail.com>
2021-12-11 17:32:48 +0100

    Checked item.

--- src/components/Tod.js
+++ src/components/Tod.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Compelted from './Compelted';
+
+
 const Tod = (props) => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const checkedHandler = (e) => {
+        const checked = e.target.checked;
+        if (checked) {
+            setIsChecked(true)
+        } else {
+            setIsChecked(false);
+        }
+    };
     return (
         <div className='card'>
             <div className='card1'>
                 <h2 style={{ textAlign: 'center' }}>{props?.data?.title}</h2>
                 <div className='actions'>
-                    <input type="checkbox" id={props.id} />
+                    <input type="checkbox" id={props.id} onClick={(e) => {
+                        checkedHandler(e);
+                    }} />
                     <button className='btnDelete' onClick={() => {
                         props.deleteHandler(props.id);
                         document.getElementById(`${props.id}`).checked = false;
                     }}>Delete</button>
                 </div>
             </div>
-            <div className='card2' >
-                <Compelted />
-            </div>
+            {isChecked &&
+                <div className='card2' >
+                    <Compelted />
+                </div>
+            }
         </div>
     )
 }
